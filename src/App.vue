40c8fdffbd3435d960606ab409816456aa1b58d8
thehuseyinoglu <thehuseyinoglu@gmail.com>
2023-01-23 02:52:42 +0300

    Create sidebar and import

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+import SiteNavigation from './components/SiteNavigation.vue';
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
